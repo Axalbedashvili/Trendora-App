6d55f415fb2cadcd2c643a57ea732a2e9fbfac39
Axalbedashvili <gorgiaxalbedashvili@gmail.com>
2026-08-24 16:34:43 +0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Component from './routes/index';
+import App from './routes/index';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Component />
+    <App />
   </React.StrictMode>
 );
